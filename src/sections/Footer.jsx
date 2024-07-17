9b94435e15078fe7a0575a0050163165843d10e0
Galih. <galihpujiirianto@gmail.com>
2024-07-17 20:32:20 +0700

    fix: add more padding on footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,20 +8,20 @@ export default function Footer() {
       sx={{ ...sectionStyle, bg: "gray.800", color: "gray.50" }}
     >
       <Heading color="teal">Sigmart</Heading>
-      <List my={2}>
-        <ListItem>
+      <List my={4}>
+        <ListItem my={1}>
           <Link href="#">Home</Link>
         </ListItem>
-        <ListItem>
+        <ListItem my={1}>
           <Link href="#about">Tentang Kami</Link>
         </ListItem>
-        <ListItem>
+        <ListItem my={1}>
           <Link href="#product">Produk Kami</Link>
         </ListItem>
-        <ListItem>
+        <ListItem my={1}>
           <Link href="#testimonial">Testimoni</Link>
         </ListItem>
-        <ListItem>
+        <ListItem my={1}>
           <Link href="#contact">Kontak Kami</Link>
         </ListItem>
       </List>

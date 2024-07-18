34268b2af3eff0ae68b9776a625845a0d4782cfd
Galih. <galihpujiirianto@gmail.com>
2024-07-18 08:41:49 +0700

    fix: set max width hero on large

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,5 @@
 import {
+  Container,
   Button,
   Center,
   Divider,
@@ -15,34 +16,38 @@ import { sectionStyle } from "../lib";
 
 export default function Hero() {
   return (
-    <SimpleGrid
-      as="section"
-      id="#"
-      columns={{ sm: 1, md: 2 }}
-      spacing={4}
-      sx={{ ...sectionStyle, minH: "30rem" }}
-    >
-      <Flex direction="column" justify="center">
-        <Heading my={2}>
-          Pilihan Hebat dan Smart?
-          <br />
-          <Highlight query="Sigmart solusinya!" styles={{ color: "teal.400" }}>
-            Sigmart solusinya!
-          </Highlight>
-        </Heading>
-        <Text my={2}>
-          Dapatkan barang-barang asli yang berkualitas, tahan lama, dan keren
-          untuk memenuhi kebutuhan sehari-harimu, di Sigmart!
-        </Text>
-        <Divider my={2} orientation="horizontal" />
-
-        <Button colorScheme="teal" leftIcon={<Icon as={FaBagShopping} />}>
-          Beli sekarang!
-        </Button>
-      </Flex>
-      <Center>
-        <Image src="/sigmart-banner.png" />
-      </Center>
-    </SimpleGrid>
+    <Center>
+      <SimpleGrid
+        as="section"
+        id="#"
+        columns={{ sm: 1, md: 2 }}
+        spacing={4}
+        sx={{ ...sectionStyle, maxW: "69rem", minH: "30rem" }}
+      >
+        <Flex direction="column" justify="center">
+          <Heading my={2}>
+            Pilihan Hebat dan Smart?
+            <br />
+            <Highlight
+              query="Sigmart solusinya!"
+              styles={{ color: "teal.400" }}
+            >
+              Sigmart solusinya!
+            </Highlight>
+          </Heading>
+          <Text my={2}>
+            Dapatkan barang-barang asli yang berkualitas, tahan lama, dan keren
+            untuk memenuhi kebutuhan sehari-harimu, di Sigmart!
+          </Text>
+          <Divider my={2} orientation="horizontal" />
+          <Button colorScheme="teal" leftIcon={<Icon as={FaBagShopping} />}>
+            Beli sekarang!
+          </Button>
+        </Flex>
+        <Center>
+          <Image src="/sigmart-banner.png" />
+        </Center>
+      </SimpleGrid>
+    </Center>
   );
 }
